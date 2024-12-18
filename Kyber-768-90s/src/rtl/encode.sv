417ce0eb6191e945276aa828ddaca99401730041
SyedHassanUlHaq <ulhaqhassan1@gmail.com>
2024-12-18 15:24:05 +0500

    [synthesizeable] encode.sv

--- Kyber-768-90s/src/rtl/encode.sv
+++ Kyber-768-90s/src/rtl/encode.sv
@@ -1,6 +1,6 @@
 module encode #(parameter D = 8, BYTE_LEN = 32) (
-    input  logic signed [15:0] F [0:255],            // Input 16-bit signed array
-    output logic [7:0] B [0:BYTE_LEN * D - 1]       // Output 8-bit byte array
+    input  logic signed [15:0] F [255:0],            // Input 16-bit signed array
+    output logic [7:0] B [BYTE_LEN * D - 1 : 0]       // Output 8-bit byte array
 );
 
     logic [(BYTE_LEN * D * 8) - 1:0] bit_array;      // Flattened bit array
@@ -14,14 +14,14 @@ module encode #(parameter D = 8, BYTE_LEN = 32) (
         .bit_array(bit_array),
         .byte_array(B)
     );
-
+    logic signed [15:0] a;
     always_comb begin
         // Initialize the bit array
         bit_array = '0;
 
         // Populate the bit array with encoded values
         for (i = 0; i < 256; i++) begin
-            logic signed [15:0] a = F[i];
+            a = F[i];
             for (j = 0; j < D; j++) begin
                 bit_array[i * D + j] = a[0];  // Extract the least significant bit
                 a = a >> 1;                  // Shift right by 1
