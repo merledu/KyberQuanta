464e69532aab46bd5cc368a192081834d23a1ba5
SyedHassanUlHaq <ulhaqhassan1@gmail.com>
2024-12-18 15:20:39 +0500

    [synthesizeable] bits_to_bytes.sv

--- Kyber-768-90s/src/rtl/bits_to_bytes.sv
+++ Kyber-768-90s/src/rtl/bits_to_bytes.sv
@@ -5,17 +5,15 @@ module bits_to_bytes #(
     parameter BYTE_LENGTH = BIT_LENGTH / 8
 ) (
     input  logic [BIT_LENGTH-1:0] bit_array,
-    output logic [7:0] byte_array [BYTE_LENGTH-1:0]  // Unpacked array of bytes
+    output logic [7:0] byte_array [BYTE_LENGTH-1:0]  
 );
 
     integer i, j;
 
     always_comb begin
-        // Loop to assign bits to bytes in little-endian format
         for (i = 0; i < BYTE_LENGTH; i = i + 1) begin
-            byte_array[i] = 8'b0; // Initialize each byte to zero
+            byte_array[i] = 8'b0; 
             for (j = 0; j < 8; j = j + 1) begin
-                // Reverse the bits within each byte
                 byte_array[i][j] = bit_array[i * 8  +( 7-j )];
             end
         end
