4f5d737ba5e483b82da505d61844228c61023951
Hamna Mohiuddin <hamnamohi2003@gmail.com>
2025-04-05 03:55:44 +0500

    Modules Changes

--- Kyber-768-90s/src/rtl/bits_to_bytes.sv
+++ Kyber-768-90s/src/rtl/bits_to_bytes.sv
@@ -14,7 +14,7 @@ module bits_to_bytes #(
         for (i = 0; i < BYTE_LENGTH; i = i + 1) begin
             byte_array[i] = 8'b0; 
             for (j = 0; j < 8; j = j + 1) begin
-                byte_array[i][j] = bit_array[i * 8  +( 7-j )];
+                byte_array[i][j] = bit_array[i * 8  +( j )];
             end
         end
     end
